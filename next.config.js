80d76bb4b0cb780d91b5d122035dc2c7b79f3b42
Dylan-Chambi <dylanchambifi@gmail.com>
2023-10-23 00:29:10 -0400

    Adding worflow with build and deploy steps

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+
+const nextConfig = {
+    basePath: '',
+    output: 'export',
+    distDir: 'build'
+}
 
 module.exports = nextConfig
